043048075be55065c795883b572277e63c660b12
LCRodrigues98 <120469256+LCRodrigues98@users.noreply.github.com>
2026-08-16 23:28:23 -0300

    app/layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,4 @@
+export const metadata = { title: 'FieldOps Pro v3.4' }
+export default function RootLayout({children}) {
+  return (<html lang="pt-br"><body style={{margin:0}}>{children}</body></html>)
+}
