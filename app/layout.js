02ccab1334d75dd04732a4ba03395f30c48aab43
LCRodrigues98 <120469256+LCRodrigues98@users.noreply.github.com>
2026-08-17 01:36:48 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,11 @@
-export const metadata = { title: 'FieldOps Pro v3.4' }
+import "./globals.css";
+
+export const metadata = { title: 'FieldOps Pro v3.4' };
+
 export default function RootLayout({children}) {
-  return (<html lang="pt-br"><body style={{margin:0, fontFamily:'system-ui'}}>{children}</body></html>)
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
 }
